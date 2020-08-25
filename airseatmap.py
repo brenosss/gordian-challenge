bc352f1685b3749feed8a5d184348c7e0800e0f9
Breno Silva <brenoe133@gmail.com>
2020-08-25 15:37:39 -0300

    Create a param to define what tags extract

--- airseatmap.py
+++ airseatmap.py
@@ -1,13 +1,14 @@
 import xml.etree.ElementTree as ET
 import argparse
 import json
+import pprint
 
 DEFAULT_FILE = 'OTA_AirSeatMapRS'
 
 
 def save_result(result, path=f'{DEFAULT_FILE}.json'):
     with open(path, 'w') as f:
-        f.write(json.dumps(result))
+        f.write(json.dumps(result, indent=4, sort_keys=True))
 
 
 def load_xml(path=f'{DEFAULT_FILE}.xml'):
@@ -16,22 +17,39 @@ def load_xml(path=f'{DEFAULT_FILE}.xml'):
     return tree.getroot(), namespaces
 
 
-def parser_seat_item(seat):
+def parser_seat_item(seat, full):
     x = {'Features': []}
     for item in seat:
         tag_without_ns = item.tag.partition('}')[-1]
         if tag_without_ns == 'Summary':
-            x[tag_without_ns] = item.attrib
+            x[tag_without_ns] = include_keys(item.attrib, full)
         elif tag_without_ns == 'Features':
             x[tag_without_ns].append(item.text)
         elif tag_without_ns == 'Status':
             x[tag_without_ns] = item.text
         elif tag_without_ns == 'Service':
-            x[tag_without_ns] = item[0].attrib['Amount']
+            x['Price'] = item[0].attrib['Amount']
     return x
 
 
-def parser_seat_planned():
+def include_keys(dictionary, full=False, keys=[]):
+    if full:
+        return dictionary
+    allowed_keys = [
+        'CabinType',
+        'Summary',
+        'SeatNumber',
+        'Price',
+        'AvailableInd',
+        'Rows',
+        'Seats',
+    ]
+    allowed_keys.extend(keys)
+    key_set = set(allowed_keys) & set(dictionary.keys())
+    return {key: dictionary[key] for key in key_set}
+
+
+def parser_seat_planned(full):
     result = []
     root, namespaces = load_xml()
     cabins = root.findall('.//ns:CabinClass', namespaces=namespaces)
@@ -40,16 +58,19 @@ def parser_seat_planned():
         for row in rows:
             seats = row.findall('./ns:SeatInfo', namespaces=namespaces)
             for seat in seats:
-                result.append({
-                    'Layout': cabin.attrib['Layout'],
-                    'RowNumber': row.attrib['RowNumber'],
-                    'CabinType': row.attrib['CabinType'],
-                    **parser_seat_item(seat),
-                    **seat.attrib
-                })
+                result.append(include_keys(
+                    {
+                        'Layout': cabin.attrib['Layout'],
+                        'RowNumber': row.attrib['RowNumber'],
+                        'CabinType': row.attrib['CabinType'],
+                        **parser_seat_item(seat, full),
+                        **seat.attrib
+                    },
+                    full=full
+                ))
     return result
 
-def parser_seat_normalized():
+def parser_seat_normalized(full):
     result = []
     root, namespaces = load_xml()
     cabins = root.findall('.//ns:CabinClass', namespaces=namespaces)
@@ -60,20 +81,42 @@ def parser_seat_normalized():
             seats = row.findall('./ns:SeatInfo', namespaces=namespaces)
             list_seats = []
             for seat in seats:
-                list_seats.append({
-                    **parser_seat_item(seat),
-                    **seat.attrib
-                })
-            list_rows.append({
-                'RowNumber': row.attrib['RowNumber'],
-                'CabinType': row.attrib['CabinType'],
-                'Seats': list_seats
-            })
+                list_seats.append(
+                    include_keys(
+                        {
+                            **parser_seat_item(seat, full),
+                            **seat.attrib
+                        },
+                        full=full
+                    )
+                )
+            list_rows.append(include_keys(
+                {
+                    'RowNumber': row.attrib['RowNumber'],
+                    'CabinType': row.attrib['CabinType'],
+                    'Seats': list_seats
+                },
+                full=full)
+            )
         result.append({
             'Layout': cabin.attrib['Layout'],
             'Rows': list_rows
         })
     return result
+
+def extract_seats_data(args):
+    if args.format == 'planned':
+        result = parser_seat_planned(args.full)
+    elif args.format == 'normalized':
+        result = parser_seat_normalized(args.full)
+    if args.output_file:
+        save_result(result, path=args.output_file)
+    else:
+        save_result(result)
+    if args.print:
+        pprint.pprint(result)
+    return result
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -89,14 +132,23 @@ def main():
         type=str,
         help='Output file name',
     )
+    parser.add_argument(
+        "--print",
+        const=True,
+        nargs='?',
+        default=False,
+        help='Print result on console'
+    )
+    parser.add_argument(
+        "--full",
+        const=True,
+        nargs='?',
+        default=False,
+        help='Extract all fields'
+    )
     args = parser.parse_args()
-    if args.format == 'planned':
-        result = parser_seat_planned()
-    elif args.format == 'normalized':
-        result = parser_seat_normalized()
-    if args.output_file:
-        save_result(result, path=args.output_file)
-    else:
-        save_result(result)
+    extract_seats_data(args)
+
+
 if __name__ == "__main__":
     main()
