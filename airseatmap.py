d9b46bfb19a23fee10e510e8118d6e88f755b80f
Breno Silva <brenoe133@gmail.com>
2020-08-25 13:08:33 -0300

    Create a new function to extract normalized data

--- airseatmap.py
+++ airseatmap.py
@@ -1,5 +1,12 @@
 import xml.etree.ElementTree as ET
+import argparse
 import json
+
+
+def load_xml(path='OTA_AirSeatMapRS.xml'):
+    tree = ET.parse('OTA_AirSeatMapRS.xml')
+    namespaces = {'ns': 'http://www.opentravel.org/OTA/2003/05/common/'}
+    return tree.getroot(), namespaces
 
 
 def parser_seat_item(seat):
@@ -17,18 +24,15 @@ def parser_seat_item(seat):
     return x
 
 
-def parsr_air_seats():
+def parser_seat_planned():
     result = []
-    tree = ET.parse('OTA_AirSeatMapRS.xml')
-    root = tree.getroot()
-    namespaces = {'ns': 'http://www.opentravel.org/OTA/2003/05/common/'}
+    root, namespaces = load_xml()
     cabins = root.findall('.//ns:CabinClass', namespaces=namespaces)
     for cabin in cabins:
         rows = cabin.findall('./ns:RowInfo', namespaces=namespaces)
         for row in rows:
             seats = row.findall('./ns:SeatInfo', namespaces=namespaces)
             for seat in seats:
-                x = {}
                 result.append({
                     'Layout': cabin.attrib['Layout'],
                     'RowNumber': row.attrib['RowNumber'],
@@ -38,4 +42,45 @@ def parsr_air_seats():
                 })
     return result
 
-result = parsr_air_seats()
+def parser_seat_normalized():
+    result = []
+    root, namespaces = load_xml()
+    cabins = root.findall('.//ns:CabinClass', namespaces=namespaces)
+    for cabin in cabins:
+        rows = cabin.findall('./ns:RowInfo', namespaces=namespaces)
+        list_rows = []
+        for row in rows:
+            seats = row.findall('./ns:SeatInfo', namespaces=namespaces)
+            list_seats = []
+            for seat in seats:
+                list_seats.append({
+                    **parser_seat_item(seat),
+                    **seat.attrib
+                })
+            list_rows.append({
+                'RowNumber': row.attrib['RowNumber'],
+                'CabinType': row.attrib['CabinType'],
+                'Seats': list_seats
+            })
+        result.append({
+            'Layout': cabin.attrib['Layout'],
+            'Rows': list_rows
+        })
+    return result
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--format',
+        type=str,
+        help='Output JSON format',
+        choices=['normalized', 'planned'],
+        default='normalized')
+    args = parser.parse_args()
+    if args.format == 'planned':
+        result = parser_seat_planned()
+    elif args.format == 'normalized':
+        result = parser_seat_normalized()
+
+if __name__ == "__main__":
+    main()
